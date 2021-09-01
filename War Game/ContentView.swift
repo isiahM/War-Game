40d9c77de23b6ffad679c5e722ed65a30aecad79
Isiah Miranda <isiahmiranda9@yahoo.com>
2021-09-01 09:24:46 -0700

    Initial Commit

---
+++ War Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War Game
+//
+//  Created by Isiah Miranda on 9/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Isiah Miranda on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
