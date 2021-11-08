779849a4efe77058793c275a1cdf31c7efd631d4
Isiah Miranda <isiahmiranda9@yahoo.com>
2021-11-08 08:51:59 -0800

    Create an initial commit

--- War Game/ContentView.swift
+++ War Game/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack(){
+           
+            Image("background").ignoresSafeArea()
+        VStack(){
+            Spacer()
+                Image("logo")
+           Spacer()
+            HStack() {
+               Spacer()
+                Image("card3")
+                Spacer()
+                Image("card4")
+                Spacer()
+               }
+            Spacer()
+                Image("dealbutton")
+            Spacer()
+            HStack(){
+                Spacer()
+                VStack(){
+                    Text("Player")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 10.0)
+                    Text("0")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                VStack(){
+                    Text("CPU")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 10.0)
+                    Text("0")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+            }
+            Spacer()
+                    }
+            }
+            
     }
 }
-
+   
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
